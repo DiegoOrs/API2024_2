5ce45d5d63eafc37791e567a8fc2a5654450755b
DiegoOrs <diegodaop20@gmail.com>
2024-11-27 09:47:55 -0500

    Descripción de los cambio

--- src/index.js
+++ src/index.js
@@ -1,47 +1,29 @@
 import express from 'express';
-import cors from 'cors'; // Importa los paquetes CORS para manejar accesos cruzados
-
-// Importa las rutas
-import usuariosRoutes from './routes/usuarios.routes.js';
-import ejerciciosRoutes from './routes/ejercicios.routes.js';
+import usuariosRoutes from './routes/usuarios.routes.js'; // Importa las rutas de usuarios
+import ejerciciosRoutes from './routes/ejercicios.routes.js'; // Importa las rutas de usuarios
 import comunidadRoutes from './routes/comunidad.routes.js';
 import motivacionesRoutes from './routes/motivaciones.routes.js';
 import notificacionesRoutes from './routes/notificaciones.routes.js';
-import rachaRoutes from './routes/racha.routes.js';
+import rachaRoutes from './routes/racha.routes.js'; // Importa las rutas de rachas
 import recompensasRoutes from './routes/recompensas.routes.js';
 import rutinasRoutes from './routes/rutinas.routes.js';
 import procesoRoutes from './routes/proceso.routes.js';
-
-// Inicializa la aplicación Express
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Configuración de CORS
-const corsOptions = {
-  origin: ['http://localhost:8100', 'https://api2024-2-2-5tn4.onrender.com'], // Solo dominios
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Métodos permitidos
-  credentials: true,
-};
-app.use(cors(corsOptions));
 
 // Middleware para parsear JSON
 app.use(express.json());
 
-// Rutas: Define un prefijo claro para cada grupo de rutas
-app.use('/api/usuarios', usuariosRoutes);
-app.use('/api/ejercicios', ejerciciosRoutes);
-app.use('/api/comunidad', comunidadRoutes);
-app.use('/api/motivaciones', motivacionesRoutes);
-app.use('/api/notificaciones', notificacionesRoutes);
+// Rutas
+app.use('/api', usuariosRoutes); // Rutas de usuarios bajo el prefijo /api
+app.use('/api', ejerciciosRoutes);
+app.use('/api', comunidadRoutes);
+app.use('/api', motivacionesRoutes);
+app.use('/api', notificacionesRoutes);
 app.use('/api/rachas', rachaRoutes);
 app.use('/api/recompensas', recompensasRoutes);
 app.use('/api/rutinas', rutinasRoutes);
 app.use('/api/procesos', procesoRoutes);
-
-// Middleware para manejar rutas no encontradas
-app.use((req, res) => {
-  res.status(404).json({ message: 'Endpoint not found' });
-});
 
 // Inicia el servidor
 app.listen(PORT, () => {
